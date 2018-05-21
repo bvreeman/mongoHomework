0bb3a6bb8985ebd59efa259e37bac7738245fa49
bvreeman <bvreeman@gmail.com>
2018-05-20 22:46:01 -0500

    got the page css working

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(`${__dirname}public`));
+app.use(express.static('public'));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
