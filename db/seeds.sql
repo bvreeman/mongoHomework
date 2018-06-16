fac33ab5644f802b9ef62c7f961a6d9856fc5038
bvreeman <bvreeman@gmail.com>
2018-06-16 09:55:46 -0500

    setting up the system

--- db/seeds.sql
+++
@@ -1,7 +0,0 @@
-USE burgers_db;
-
-INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom and Swiss', false);
-
-INSERT INTO burgers (burger_name, devoured) VALUES ('California Burger', false);
-
-INSERT INTO burgers (burger_name, devoured) VALUES ('Buffalo Burger', false);
