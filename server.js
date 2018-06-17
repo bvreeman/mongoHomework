19edaf9ff164b1f8c733dccae06c837435ebf224
bvreeman <bvreeman@gmail.com>
2018-06-17 10:08:15 -0500

    setting up handlebars, not totally set up yet

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const bodyParser = require('body-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
 // It works on the client and on the server
-let axios = require('axios');
-let cheerio = require('cheerio');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 // Require all models
 const db = require('./models');
@@ -17,20 +17,36 @@ const db = require('./models');
 // Initialize Express
 const app = express();
 
-// Configure middleware
-
-// Use morgan logger for logging requests
-app.use(logger('dev'));
-// Use body-parser for handling form submissions
-app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static('public'));
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines';
+// parse application/text
+app.use(bodyParser.text());
 
-// Set mongoose to leverage built in JavaScript ES6 Promises
-// Connect to the Mongo DB
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+// Set Handlebars.
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Use morgan logger for logging requests
+app.use(logger('dev'));
+
+// // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+// const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines';
+
+// // Set mongoose to leverage built in JavaScript ES6 Promises
+// // Connect to the Mongo DB
+// mongoose.Promise = Promise;
+// mongoose.connect(MONGODB_URI);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log(`Server listening on: http://localhost:${PORT}`);
+});
+
