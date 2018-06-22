8c5246202c57e1c3f78405845a4a045682cf7839
bvreeman <bvreeman@gmail.com>
2018-06-22 14:59:21 -0500

    working on notes modal

--- server.js
+++ server.js
@@ -141,27 +141,60 @@ app.get('/articles/:id', function(req, res) {
     });
 });
 
-// Route for saving/updating an Article's associated Note
-app.post('/articles/:id', function(req, res) {
+// // Route for saving/updating an Article's associated Note
+// app.post('/articles/:id', function(req, res) {
+//   // Create a new note and pass the req.body to the entry
+//   db.Note.create(req.body)
+//     .then(function(dbNote) {
+//       // If a Note was created successfully, find one Article with an `_id` equal to
+//       // `req.params.id`. Update the Article to be associated with the new Note
+//       // { new: true } tells the query that we want it to return the updated User -- it
+//       // returns the original by default
+//       // Since our mongoose query returns a promise, we can chain another `.then` which
+//       // receives the result of the query
+//       return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+//     })
+//     .then(function(dbArticle) {
+//       // If we were able to successfully update an Article, send it back to the client
+//       res.json(dbArticle);
+//     })
+//     .catch(function(err) {
+//       // If an error occurred, send it to the client
+//       res.json(err);
+//     });
+// });
+
+// Create a new note
+app.post('/notes/save/:id', function(req, res) {
   // Create a new note and pass the req.body to the entry
-  db.Note.create(req.body)
-    .then(function(dbNote) {
-      // If a Note was created successfully, find one Article with an `_id` equal to
-      // `req.params.id`. Update the Article to be associated with the new Note
-      // { new: true } tells the query that we want it to return the updated User -- it
-      // returns the original by default
-      // Since our mongoose query returns a promise, we can chain another `.then` which
-      // receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-    })
-    .then(function(dbArticle) {
-      // If we were able to successfully update an Article, send it back to the client
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
+  const newNote = new db.Note({
+    body: req.body.text,
+    article: req.params.id,
+  });
+  console.log(req.body);
+  // And save the new note the db
+  newNote.save(function(error, note) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    // Otherwise
+    else {
+      // Use the article id to find and update it's notes
+      db.Article.findOneAndUpdate({ _id: req.params.id }, { $push: { notes: note } })
+      // Execute the above query
+        .exec(function(err) {
+          // Log any errors
+          if (err) {
+            console.log(err);
+            res.send(err);
+          } else {
+            // Or send the note to the browser
+            res.send(note);
+          }
+        });
+    }
+  });
 });
 
 app.get('/saved', function(req, res) {
